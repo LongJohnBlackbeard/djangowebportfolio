990ee2b2a2fc0f86971256de59a157b1df6b75fa
Daniel <72884152+LongJohnBlackbeard@users.noreply.github.com>
2022-08-04 07:53:06 -0500

    aded postgres

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -85,8 +85,13 @@ WSGI_APPLICATION = 'portfolio.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'dtujo-portfolio',
+        'USER': 'dtujo',
+        'PASSWORD': os.getenv('DB_PASSWORD'),
+        'HOST': os.getenv('DB_HOST'),
+        'PORT': '5432',
+        "OPTIONS": {"sslmode": "require"},
     }
 }
 
