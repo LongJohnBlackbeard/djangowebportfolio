00f35cabc85635cc019d9476f3155d4efd91e480
Daniel <72884152+LongJohnBlackbeard@users.noreply.github.com>
2022-08-07 21:48:42 -0500

    added new domain to allowed hosts for real this time

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = os.getenv('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['danieltujo.azurewebsites.net']
+ALLOWED_HOSTS = ['danieltujo.azurewebsites.net', 'danieltujo.com']
 CSRF_TRUSTED_ORIGINS = [
     'https://danieltujo.azurewebsites.net', 'danieltujo.com']
 
